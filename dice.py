3e1cc6fcd297579272597a89a449db3ac1bf131e
Jun Woo Shin <shin.junwoo97@gmail.com>
2015-10-27 08:09:30 -0400

    initial

---
+++ dice.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+length = int(sys.argv[1])
+digits = []
+
+for word in range(length):
+	this_word = ''
+	for dig in range(5):
+		os.urandom(1)
+
+	digits.append(int(this_word))
