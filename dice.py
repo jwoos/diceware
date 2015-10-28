bc21aaa0b3bf5538ac422561b375118e0de9a49e
Jun Woo Shin <shin.junwoo97@gmail.com>
2015-10-28 16:02:43 -0400

    numbers generated

--- dice.py
+++ dice.py
@@ -1,12 +1,16 @@
-import os
 import sys
+from random import SystemRandom
 
 length = int(sys.argv[1])
 digits = []
 
+random_dig = SystemRandom()
+
 for word in range(length):
 	this_word = ''
 	for dig in range(5):
-		os.urandom(1)
-
+		a_dig = random_dig.randrange(1, 7)
+		this_word += str(a_dig)
 	digits.append(int(this_word))
+
+print(digits)
---
+++ test_random.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+numbers = []
+
+for _ in range(100000):
+	ord(os.urandom(1))
